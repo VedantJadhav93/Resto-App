ef625876789444cfca7148606cba9fcd781067cc
Vedant Jadhav <131908596+VedantJadhav93@users.noreply.github.com>
2024-03-26 23:39:01 +0530

    added images,rates
    xx

--- foodi-client/src/Components/Cards.jsx
+++ foodi-client/src/Components/Cards.jsx
@@ -7,13 +7,13 @@ const Cards = ({item}) => {
     
         <div className="card w-96 bg-base-100 shadow-xl">
             <Link to={'/menu/${item._id}'}>
-            <figure><img src={item.imgage} alt="Images" className='hover:scale-105 transition-all duration-200 md:h-72'/></figure>
+            <figure><img src={item.image} alt="Images" className='hover:scale-105 transition-all duration-200 md:h-72'/></figure>
             </Link>
             <div className="card-body">
                 <h2 className="card-title">{item.name}</h2>
                 <p>Description of the item</p>
                 <div className="card-actions justify-between items-center mt-2">
-                    <h5 className='font-semibold'><span className='text-sm text-red'>Rs. </span>{item.price}</h5>
+                    <h5 className='font-semibold'><span className='text-sm text-red'>₹</span>{item.price}</h5>
                     <button className="btn bg-green text-white">Buy Now</button>
                 </div>
             </div>
